d8e7e49c7d61f00fa4e5a53ef1d3a43fdf3b0a97
DIWA123 <diwakarkumarsun@gmail.com>
2016-04-02 16:57:40 +0530

    my file
    nothing

---
+++ 2.py
@@ -0,0 +1,6 @@
+list = [1, 2, 3, 4, 5, 6, 7]
+def middle(t) :
+  return t[1:-1]
+# print results
+print 'The original list: ', list
+print "The new list produced by the 'middle' function:" , middle(list)
